4a328b4252728217670bd103577f7698e4256492
srd785 <107203103+srd785@users.noreply.github.com>
2023-06-15 23:45:46 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIAWQQ2VW7QLKS7P27K"
-  secret_key = "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"
-  region     = "us-west-2"
+  access_key = "AKIAWQQ2VW7QNH7KJD4K"
+  secret_key = "GkGSZRxnyh2HUWbF5cjfw2Wm/CypD/3XLCvt6G/q"
+  region     = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-076bca9dd71a9a578"
+  ami           = "ami-03cb1380eec7cc118"
   instance_type = "t2.micro"
 
   tags = {
