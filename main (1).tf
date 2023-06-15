6af17bb030f9c2dcb3774db6ea3a7ede2d6f807b
srd785 <107203103+srd785@users.noreply.github.com>
2023-06-15 14:44:36 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -3,8 +3,6 @@ provider "aws" {
   secret_key = "Z85K00xTx/CXdoa2NKSUB1fS0zHwXa2WbTO2SuOL"
   region     = "us-west-2"
 }
-
-
 resource "aws_instance" "example" {
   ami           = "ami-076bca9dd71a9a578"
   instance_type = "t2.micro"
