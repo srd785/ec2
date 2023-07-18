f674de8bb916d8a06105ab4614218090e0b0a3ae
Abhinai20 <133314950+Abhinai20@users.noreply.github.com>
2023-07-18 20:20:53 +0530

    Update main.sh
    Updated execute permission

--- harness/main.sh
+++ harness/main.sh
@@ -5,9 +5,9 @@ sudo snap install docker
 sudo snap install terraform
 sudo snap install helm
 
+chmod +x helm.sh k8s.yaml terra.tf
+
 sudo sh helm.sh
-
 sudo kubectl apply -f k8s.yaml
-
 sudo terraform init
 sudo terraform apply --auto-approve
