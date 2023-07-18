0cd6549b3b483f2b4866addccea82a458015579f
srd785 <107203103+srd785@users.noreply.github.com>
2023-07-18 21:33:25 +0530

    Update main.sh

--- harness/main.sh
+++ harness/main.sh
@@ -5,9 +5,10 @@ sudo snap install docker
 sudo snap install terraform
 sudo snap install helm
 
-chmod +x helm.sh k8s.yaml terra.tf
+chmod +x helm.sh k8s.yaml terra.tf docker_delegate.sh
 
 sudo sh helm.sh
 sudo kubectl apply -f k8s.yaml
 sudo terraform init
 sudo terraform apply --auto-approve
+sudo sh docker_delegate.sh
