36207dab309e69a1fcb70f892db18028d27742c4
Abhinai20 <133314950+Abhinai20@users.noreply.github.com>
2023-07-18 20:19:56 +0530

    Update main.sh
    Update sudo access

--- harness/main.sh
+++ harness/main.sh
@@ -1,7 +1,13 @@
 #!/bin/bash/
-sh helm.sh
 
-kubectl apply -f k8s.yaml
+#Installing the packages
+sudo snap install docker 
+sudo snap install terraform
+sudo snap install helm
 
-terraform init
-terraform apply --auto-approve
+sudo sh helm.sh
+
+sudo kubectl apply -f k8s.yaml
+
+sudo terraform init
+sudo terraform apply --auto-approve
