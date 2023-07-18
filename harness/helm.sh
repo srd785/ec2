f7db58a194cb454f86ea9de46da0ec6d05320972
srd785 <107203103+srd785@users.noreply.github.com>
2023-07-18 16:16:57 +0530

    Update helm.sh

--- harness/helm.sh
+++ harness/helm.sh
@@ -5,12 +5,11 @@ helm repo add harness-delegate https://app.harness.io/storage/harness-download/d
 helm repo update harness-delegate
 
 #Now we are ready to install the delegate
-helm upgrade -i helm-delegate --namespace harness-delegate-helm --create-namespace \
-  harness-delegate/harness-delegate-helm \
+helm upgrade -i helm-delegate --namespace harness-delegate-ng --create-namespace \
+  harness-delegate/harness-delegate-ng \
   --set delegateName=helm-delegate \
   --set accountId=LZyeo82AR5KUT0J6P2mtZg \
   --set delegateToken=MzQ4NDUyYjc3MzlhM2Q5ODUwOGFlZDkyMGQxOTBiM2I= \
   --set managerEndpoint=https://app.harness.io/gratis \
   --set delegateDockerImage=harness/delegate:23.06.79707 \
   --set replicas=1 --set upgrader.enabled=false
-
