a8f41e9abaa0189c8f40d50c8f3e7121cf5fd771
srd785 <107203103+srd785@users.noreply.github.com>
2023-07-18 15:09:46 +0530

    Create helm.sh

---
+++ harness/helm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Add Harness helm chart repo to your local helm registry
+helm repo add harness-delegate https://app.harness.io/storage/harness-download/delegate-helm-chart/
+
+helm repo update harness-delegate
+
+#Now we are ready to install the delegate
+helm upgrade -i helm-delegate --namespace harness-delegate-ng --create-namespace \
+  harness-delegate/harness-delegate-ng \
+  --set delegateName=helm-delegate-infra \
+  --set accountId=LZyeo82AR5KUT0J6P2mtZg \
+  --set delegateToken=MzQ4NDUyYjc3MzlhM2Q5ODUwOGFlZDkyMGQxOTBiM2I= \
+  --set managerEndpoint=https://app.harness.io/gratis \
+  --set delegateDockerImage=harness/delegate:23.06.79707 \
+  --set replicas=1 --set upgrader.enabled=false
+
