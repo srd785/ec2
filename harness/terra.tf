58f30c45b6a6253b6b7370d7e26d93e2678d6d77
srd785 <107203103+srd785@users.noreply.github.com>
2023-07-18 15:12:52 +0530

    Create terra.tf

---
+++ harness/terra.tf
@@ -0,0 +1,20 @@
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.1.7"
+
+  account_id = "LZyeo82AR5KUT0J6P2mtZg"
+  delegate_token = "MzQ4NDUyYjc3MzlhM2Q5ODUwOGFlZDkyMGQxOTBiM2I="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-infra"
+  manager_endpoint = "https://app.harness.io/gratis"
+  delegate_image = "harness/delegate:23.06.79707"
+  replicas = 1
+  upgrader_enabled = false
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
